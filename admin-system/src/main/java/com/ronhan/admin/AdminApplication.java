42ab2a45f418f6509d93abd4bfb2abd9d9878492
QingDong Zhang <qingdong.zhang@ronhan.com>
2020-07-17 18:51:08 +0800

    system module

---
+++ admin-system/src/main/java/com/ronhan/admin/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.ronhan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author qingdong.zhang
+ * @version 1.0
+ * @since 2020-07-17 17:38
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class);
+    }
+
+}
---
+++ admin-system/src/main/java/com/ronhan/admin/modules/data/mybatis/MyBatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.ronhan.admin.modules.data.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlusConfig
+ *
+ * @author qingdong.zhang
+ * @version 1.0
+ * @since 2020-07-17 18:00
+ */
+@Configuration
+@MapperScan({"com.ronhan.admin.**.mapper"})
+public class MyBatisPlusConfig {
+
+
+
+}
